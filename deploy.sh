af552017f69e85e9a578bb551aac9e6112cf9f93
kHigasa <kojihigasa@gmail.com>
2024-11-03 19:57:03 +0900

    rebuilding site Sun Nov  3 19:57:03 JST 2024

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t hugo-tranquilpeak-theme
+hugo -t hugo-theme-stack
 
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuilding site $(date)"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
